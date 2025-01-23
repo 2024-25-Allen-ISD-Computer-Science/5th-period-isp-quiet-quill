fe6166ef8f25e5bd47bd62bd7e5016932764cf55
Rey Perez <111249744+ReyMPerez@users.noreply.github.com>
2025-01-23 02:50:07 +0000

    Adds dynamic background to login and signup pages
    *Added feature to display moving particles on the login.html and signup.html files
    *Created the particles-config.js file to handle the styles of the particles of the dynamic background
    *Connected the "Log Out" button on the Profile page to the login.html file
    *Created main.js to handle user information retrieval
    *Relates to Task #2

---
+++ main.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDtWQvR0g7glI6UmARjzbPqbqWor-1RGbI",
+    authDomain: "quiet-quill-d2bf0.firebaseapp.com",
+    projectId: "quiet-quill-d2bf0",
+    storageBucket: "quiet-quill-d2bf0.firebasestorage.app",
+    messagingSenderId: "722653771174",
+    appId: "1:722653771174:web:d830bad6b8adb07b9e0b67"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
---
+++ particles-config.js
@@ -0,0 +1,36 @@
+particlesJS('particles-js', {
+    particles: {
+        number: {
+            value: 50,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        shape: {
+            type: 'circle', // You can also use 'polygon', 'edge', etc.
+            stroke: {
+                width: 0,
+                color: '#000000'
+            }
+        },
+        opacity: {
+            value: 0.9,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 0.3,
+                opacity_min: 0.1,
+                sync: false
+            }
+        },
+        move: {
+            enable: true,
+            speed: 1,
+            direction: 'none',
+            random: false,
+            straight: false,
+            out_mode: 'out'
+        }
+    }
+});
